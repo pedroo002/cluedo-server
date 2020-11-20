1ff867c2bf275d659fffa749984aef614707badc
pedroo002 <orosz401@gmail.com>
2020-11-20 21:15:38 +0100

    Update server

--- index.js
+++ index.js
@@ -223,8 +223,6 @@ app.post('/player', async (req, res) => {
                 res.status(500).json(error);
             }
         })
-
-        currentPlayer = newPlayer;
         res.status(201).json(newPlayer);
     })
 })
@@ -247,7 +245,6 @@ app.put('/login-player', async (req, res) => {
                 player.logged_in = true;
                 try {
                     await player.save();
-                    currentPlayer = player;
                     res.status(200).json(player);
                 } catch (error) {
                     res.status(500).json({ message: error.message });
@@ -394,26 +391,62 @@ async function getPlayer(req, res, next) {
 }
 
 app.post('/incriminate', (req, res) => {
-    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    let payload =
+    {
+        player_id: req.body.player_id,
+        room: req.body.room,
+        tool: req.body.tool,
+        suspect: req.body.suspect
+    }
     pusher.trigger(req.query.channel_name, 'incrimination', payload);
     res.sendStatus(200);
 })
 
 app.post('/accuse', (req, res) => {
-	let payload = {message: req.body.message, sender_id: req.body.sender_id}
+	let payload =
+    {
+        player_id: req.body.player_id,
+        room: req.body.room,
+        tool: req.body.tool,
+        suspect: req.body.suspect
+    }
     pusher.trigger(req.query.channel_name, 'accusation', payload);
     res.sendStatus(200);
 })
 
 app.post('/move', (req, res) => {
-	let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    let payload = 
+    {
+        player_id: req.body.player_id,
+        target_position:
+        {
+            row: req.body.target_position.row,
+            col: req.body.target_position.col
+        }
+    }
     pusher.trigger(req.query.channel_name, 'player-moves', payload);
     res.sendStatus(200);
 })
 
 app.post('/draw-card', (req, res) => {
-	let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    let payload =
+    {
+        player_id: req.query.player_id,
+        card_name: req.query.card_name
+    }
     pusher.trigger(req.query.channel_name, 'card-drawing', payload);
+    res.sendStatus(200);
+})
+
+app.post('/dice', (req, res) => {
+    let payload = 
+    {
+        player_id: req.body.player_id,
+        first_value: req.body.first_value,
+        second_value: req.body.second_value,
+        extra_value: req.body.extra_value
+    }
+    pusher.trigger(req.query.channel_name, 'dice-event', payload);
     res.sendStatus(200);
 })
 
@@ -493,13 +526,33 @@ app.post('/player-arrives', (req, res) => {
     res.sendStatus(200);
 })
 
+app.post('/new-player-added', (req, res) => {
+    pusher.trigger(req.query.channel_name, 'new-player-added', {});
+    res.sendStatus(200);
+})
+
+app.post('/refresh-multi-selector', (req, res) => {
+    for (let i = 0; i < req.body.selection_count; i++) {
+        let payload =
+        {
+            message:
+            {
+                player_name: req.body.selections[i].player_name,
+                character_name: req.body.selections[i].character_name,
+                token_src: req.body.selections[i].token_src
+            }
+        }
+        pusher.trigger(req.query.channel_name, 'character-selected', payload);
+    }
+    res.sendStatus(200);
+})
+
 app.post('/mystery-pairs', (req, res) => {
     let payload = 
     {
         message:
         {
-            message: req.body.message,
-            pair_list: req.body.mystery_pairs
+            pairs: req.body.message.pairs
         },
         sender_id: req.body.sender_id
     }
@@ -508,12 +561,139 @@ app.post('/mystery-pairs', (req, res) => {
 })
 
 app.post('/ready-to-game', (req, res) => {
-    let payload =
-    {
-        message: req.body.message,
-        sender_id: req.body.sender_id
+    pusher.trigger(req.query.channel_name, 'ready-to-game', req.query.player_name);
+    res.sendStatus(200);
+})
+
+app.post('/fetch-cards', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id};
+    pusher.trigger(req.query.channel_name, 'fetch-cards', payload);
+    res.sendStatus(200);
+})
+
+app.post('/map-loaded', async (req, res) => {
+    var channel = await Channel.findOne({
+        channel_name: req.query.channel_name
+    },
+    (err) => {
+        if (err) {
+            res.status(500).json({ message: err.message });
+        }
+    })
+
+    if (channel) {
+        channel.loaded_users = channel.loaded_users + 1;
+        try {
+            await channel.save();
+            if (channel.loaded_users == channel.subscribed_users.length) {
+                let payload = {message: req.body.message, sender_id: req.body.sender_id};
+                pusher.trigger("presence-" + req.query.channel_name, 'map-loaded', payload);
+            }
+            res.sendStatus(200);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
     }
-    pusher.trigger(req.query.channel_name, 'ready-to-game', payload);
+    else {
+        res.status(404).json({ message: 'Channel not found.' });
+    }
+})
+
+app.post('/mystery-cards-activity-loaded', async (req, res) => {
+    var channel = await Channel.findOne({
+        channel_name: req.query.channel_name
+    },
+    (err) => {
+        if (err) {
+            res.status(500).json({ message: err.message });
+        }
+    })
+
+    if (channel) {
+        channel.loaded_users = channel.loaded_users + 1;
+        try {
+            await channel.save();
+            if (channel.loaded_users == channel.subscribed_users.length) {
+                let payload = {message: req.body.message, sender_id: req.body.sender_id};
+                pusher.trigger("presence-" + req.query.channel_name, 'fetch-cards', payload);
+                channel.loaded_users = 0;
+                await channel.save();
+            }
+            res.sendStatus(200);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+    else {
+        res.status(404).json({ message: 'Channel not found.' });
+    }
+})
+
+app.post('/dark-cards-ready', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    pusher.trigger(req.query.channel_name, 'dark-cards-ready', payload);
+    res.sendStatus(200);
+})
+
+app.post('/dark-cards-close', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    pusher.trigger(req.query.channel_name, 'dark-cards-close', payload);
+    res.sendStatus(200);
+})
+
+app.post('/throw-helper-card', (req, res) => {
+    let payload = 
+    {
+        player_id: req.query.player_id,
+        card_name: req.query.card_name
+    }
+    pusher.trigger(req.query.channel_name, 'helper-card-thrown', payload);
+    res.sendStatus(200);
+})
+
+app.post('/incrimination-details-ready', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    pusher.trigger(req.query.channel_name, 'incrimination-details-ready', payload);
+    res.sendStatus(200);
+})
+
+app.post('/trigger-card-reveal', (req, res) => {
+    let payload = req.query.player_id;
+    pusher.trigger(req.query.channel_name, 'card-reveal-obligation', payload);
+    res.sendStatus(200);
+})
+
+app.post('/show-helper-card', (req, res) => {
+    let payload = 
+    {
+        player_id: req.query.player_id,
+        card_name: req.query.card_name
+    }
+    pusher.trigger(req.query.channel_name, 'helper-card-shown', payload);
+    res.sendStatus(200);
+})
+
+app.post('/skip-reveal', (req, res) => {
+    let payload = req.query.player_id;
+    pusher.trigger(req.query.channel_name, 'skip-reveal', payload);
+    res.sendStatus(200);
+})
+
+app.post('/no-card', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    pusher.trigger(req.query.channel_name, 'nobody-showed-card', payload);
+    res.sendStatus(200);
+})
+
+app.post('/incrimination-finished', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    pusher.trigger(req.query.channel_name, 'incrimination-finished', payload);
+    res.sendStatus(200);
+})
+
+app.post('/note-closed', (req, res) => {
+    let payload = {message: req.body.message, sender_id: req.body.sender_id}
+    pusher.trigger(req.query.channel_name, 'note-closed', payload);
     res.sendStatus(200);
 })
 
